e9d5e0486d60acc43fa53354c1ffa4360ee29880
rodrigosilva32 <55798409+rodrigoSilva23@users.noreply.github.com>
2020-11-29 15:25:53 -0300

    estudo de estrutura de decisao

---
+++ atividade02.py
@@ -0,0 +1,14 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+
+#entrada de dados
+
+valor = int(input('informe um valor, tanto negavito ou positivo '))
+
+
+#condicional de verificação e exibição de dados
+if valor <0:
+    print('o valor digitado é negativo')
+
+else:
+    print('o valor digitado é positivo')
---
+++ atividade03.py
@@ -0,0 +1,20 @@
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+
+#entrada de dados 
+
+sexo = input("Informe seu Sexo F-Feminino ou M-Masculino ou N-não definido:").upper()
+
+#condicional de verificação e exibição
+
+if sexo == "F":
+    print("Sexo Feminino")
+
+elif sexo =="M":
+    print("Sexo Masculino")
+
+elif sexo =="N":
+    print("Sexo Não definido")
+
+else:
+    print("Por favor informe um Sexo valido!")
