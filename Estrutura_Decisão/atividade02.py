8113cc5dd0d6bb2c0780d5ee6226eeb7f3556340
rodrigosilva32 <55798409+rodrigoSilva23@users.noreply.github.com>
2020-11-29 14:52:22 -0300

    Add files via upload

---
+++ Estrutura_Decisão/atividade02.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor = int(input('informe um valor, tanto negavito ou positivo '))
+
+if valor <0:
+    print('o valor digitado é negativo')
+
+else:
+    print('o valor digitado é positivo')
